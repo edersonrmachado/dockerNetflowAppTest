e595d6cdead79b1152ca3593fde8e84949db0427
edersonrmachado <edersonrmachado@gmail.com>
2020-03-09 13:30:42 -0300

    Only application deployment test

--- sequenceOfTests.sh
+++ sequenceOfTests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+bash onlyApp.sh 1 2 onlyApp1x2.csv
 #bash onlyApp.sh 1 35 onlyApp1x.csv
-#bash onlyApp.sh 2 35 onlyAppB2x.csv
+#bash onlyApp.sh 2 35 onlyAppB2x.cs v
 #bash onlyApp.sh 4 35 onlyAppB4x.csv
 #bash onlyApp.sh 8 35 onlyAppB8x.csv
-bash onlyApp.sh 16 35 onlyApp16x.csv
+#bash onlyApp.sh 16 35 onlyApp16x.csv
